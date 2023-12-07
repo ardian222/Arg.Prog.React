ab46e407782f419508805c2789c554acd1b47c3d
adrian222 <ardipedroza@gmail.com>
2023-12-07 17:41:52 -0300

    first commit

---
+++ src/routes/cafe.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TittleBar from "../components/titleBar/tittleBar";
+function CafePage() {
+  return (
+    <div>
+      <TittleBar text={"Cafe"}></TittleBar>
+      CafePage
+    </div>
+  );
+}
+
+export default CafePage;
---
+++ src/routes/home.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Banner from "../components/banner/banner";
+import banner1 from "../assets/img/banner_1.png";
+import banner2 from "../assets/img/banner_2.png";
+function HomePage() {
+
+  return (
+    <div>
+      <Banner
+        img={banner1}
+        title={"¡PUMPKINLICIOUS!"}
+        description={
+          "Una temporada con tu favorito de siempre. ¡Nuestro Pumpkin Spice vuelve por tiempo limitado en sus versiones Iced, Latte y Frappuccino!"
+        }
+        backgroundColor={"#fe5000"}
+        color={"#fff"}
+        height={"450"}
+      />
+      <br />
+      <Banner
+        img={banner2}
+        title={"Unite a algo más grande"}
+        description={"Revisa las oportunidades que tenemos para vos"}
+        backgroundColor={"#d4e9e2"}
+        color={"#00754a"}
+        height={"350"}
+        isFlipped={true}
+        haveButton={true}
+      />
+    </div>
+  );
+}
+
+export default HomePage;
